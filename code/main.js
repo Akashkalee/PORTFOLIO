d0bb2949932e58733329df380fa790c293e8d436
Akashkalee <86045023+Akashkalee@users.noreply.github.com>
2023-12-09 21:14:02 +0000

    about section added

--- code/main.js
+++ code/main.js
@@ -25,5 +25,21 @@ function scrollHeader() {
 
 window.addEventListener('scroll', scrollHeader);
 
+/* Hero type effect */
+const typed = document.querySelector('.typed');
+
+if(typed) {
+    let typed_strings = typed.getAttribute('data-typed-items');
+    typed_strings = typed_strings.split(',');
+    new typed('.typed', {
+        strings: typed_strings,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000
+    });
+}
+
+
 
 
